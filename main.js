487ae8496b2fedeb8b1cb7345e9d01e106f123c9
Hanh20021706 <hanhs8155@gmail.com>
2023-03-16 09:57:06 +0700

    fix bug video

--- main.js
+++ main.js
@@ -15,15 +15,16 @@ clickMenun.addEventListener("click", function () {
 const btnVideo = document.querySelector('.btn-video__open');
 const wrapVideo = document.querySelector('.video-list');
 const closeVideo = document.querySelector('.video-item__close');
-const bodyVideo = document.querySelector("body")
+const bodyVideo = document.querySelector('.video-wrap__color')
 
 
 btnVideo.addEventListener('click', ()=> {
     console.log(wrapVideo, 'wrapVideo');
     wrapVideo.classList.add('show-video')
-
+    bodyVideo.classList.add('video-coating')
 })
 
 closeVideo.addEventListener('click', ()=> {
     wrapVideo.classList.remove('show-video')
+    bodyVideo.classList.remove('video-coating')
 })
